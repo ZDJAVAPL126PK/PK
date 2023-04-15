ee982259a1c021c7f3f13edb9fee862331868268
szjozi <szjozefczuk.sda@gmail.com>
2023-04-15 11:44:02 +0200

    init

---
+++ src/test/java/com/sda/clinicapi/ClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.clinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
