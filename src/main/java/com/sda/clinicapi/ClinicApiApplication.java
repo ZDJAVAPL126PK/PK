5859de789ab8b4a6905c315140020a1d4aa58cbb
szjozi <szjozefczuk.sda@gmail.com>
2023-04-15 13:27:34 +0200

    flyway config added

--- src/main/java/com/sda/clinicapi/ClinicApiApplication.java
+++ src/main/java/com/sda/clinicapi/ClinicApiApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicApiApplication {
 
     public static void main(String[] args) {
-        log.info("Test message");
         SpringApplication.run(ClinicApiApplication.class, args);
     }
 
