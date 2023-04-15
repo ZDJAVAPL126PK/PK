e101dedd1335bfcaecb4086df5cd8f84b5eb0aed
szjozi <szjozefczuk.sda@gmail.com>
2023-04-15 12:54:06 +0200

    logging configuration added

--- src/main/java/com/sda/clinicapi/ClinicApiApplication.java
+++ src/main/java/com/sda/clinicapi/ClinicApiApplication.java
@@ -1,12 +1,15 @@
 package com.sda.clinicapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ClinicApiApplication {
 
     public static void main(String[] args) {
+        log.info("Test message");
         SpringApplication.run(ClinicApiApplication.class, args);
     }
 
