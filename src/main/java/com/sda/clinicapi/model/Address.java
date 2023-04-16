3fe6fe130561ecba5b4708e315ba1f9f4cf39187
Tomasz Rochala <94705724+wartego@users.noreply.github.com>
2023-04-16 12:41:03 +0200

    DT-15-create-address-model (#10)
    * DT-15-create-address-model
    
    * DT-15-create-address-model generated value
    
    * DT-15-create-address-model SQL query
    
    * DT-19-create-doctor-model SQL query

---
+++ src/main/java/com/sda/clinicapi/model/Address.java
@@ -0,0 +1,31 @@
+package com.sda.clinicapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "addresses")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    @Column(nullable = false, length = 50)
+    private String street;
+
+    @Column(nullable = false, length = 25)
+    private String postCode;
+
+}
+
