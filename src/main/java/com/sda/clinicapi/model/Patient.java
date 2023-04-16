084e8c3f8edf9488a4f8fe3b8126726cc8e3e7db
MaciekC <inz.maciej.cichonski@gmail.com>
2023-04-16 12:37:47 +0200

    patient model - modified table name - small letters 2

--- src/main/java/com/sda/clinicapi/model/Patient.java
+++ src/main/java/com/sda/clinicapi/model/Patient.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
     @Column(length = 50, nullable = false)
