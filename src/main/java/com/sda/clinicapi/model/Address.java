ce60907f82ad383ff9f71eafd6b73d8036a824b5
Tomek <wartego@wp.pl>
2023-04-16 11:59:41 +0200

    DT-15-create-address-model generated value

--- src/main/java/com/sda/clinicapi/model/Address.java
+++ src/main/java/com/sda/clinicapi/model/Address.java
@@ -1,9 +1,6 @@
 package com.sda.clinicapi.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +15,7 @@ import lombok.Setter;
 public class Address {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 100)
