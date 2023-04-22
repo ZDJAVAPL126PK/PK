bf8b0cf96b81a1f6df066f914ff2062a6b764b02
szjozi <104274797+szjozi@users.noreply.github.com>
2023-04-22 14:00:17 +0200

    Dt 26 create api for users management (#26)
    * DT-27-add-endpoint-for-creating-users
    
    * refactor
    
    * password hashing fix
    
    * delete endpoint added, logging for UserService implemented
    
    * Endpoints for listing users added.
    
    * DT-30 Endpoint for updating user added.

--- src/main/java/com/sda/clinicapi/controller/UserController.java
+++ src/main/java/com/sda/clinicapi/controller/UserController.java
@@ -2,25 +2,75 @@ package com.sda.clinicapi.controller;
 
 import com.sda.clinicapi.dto.UserDTO;
 import com.sda.clinicapi.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Users Controller")
 @RequestMapping("/api/users")
+@PreAuthorize("hasRole('ADMIN')")
 @SecurityRequirement(name = "basicAuth")
 public class UserController {
 
     private final UserService userService;
 
+    // HTTP Methods
+    // =============================
+    // GET -> pobieramy zasoby  200 OK
+    // POST -> tworzymy zasoby  201 CREATED
+    // PUT -> całościowy update zasobów 201 CREATED
+    // PATCH (Łatka) -> częściowy update zasobu 201 CREATED
+    // DELETE -> usuwanie zasobów 204 NO_CONTENT
+    // HEAD -> pobranie nagłówków
+    // OPTIONS -> pobiera opcje, które mogą być wykonane na konkretnym zasobie
+
+    // https://example.com?pageNo=0&pageSize=10  - Request Param
+    @GetMapping
+    @Operation(summary = "Method is being used to get list of all users.")
+    public ResponseEntity<List<UserDTO>> findAll(
+            @RequestParam(required = false, defaultValue = "0") int pageNo,
+            @RequestParam(required = false, defaultValue = "500") int pageSize,
+            @RequestParam(required = false, defaultValue = "ASC") Sort.Direction direction,
+            @RequestParam(required = false, defaultValue = "username") String sortColumn) {
+
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, direction, sortColumn);
+        return ResponseEntity.ok(userService.findAll(pageRequest));
+    }
+
+    @GetMapping("{username}")
+    @Operation(summary = "Method is being used to get user with given username.")
+    public ResponseEntity<UserDTO> findUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userService.findUserByUsername(username));
+    }
+
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Method is being used to create new user.")
     public void create(@RequestBody UserDTO userDTO) {
         userService.create(userDTO);
     }
 
+    @DeleteMapping("{username}")
+    @Operation(summary = "Method is being used to delete user with given username.")
+    public ResponseEntity<Void> delete(@PathVariable String username) {
+        userService.delete(username);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{username}")
+    @Operation(summary = "Method is being used to update existing user.")
+    public void update(@RequestBody UserDTO userDTO, @PathVariable String username) {
+        userService.update(username, userDTO);
+    }
 }
--- src/main/java/com/sda/clinicapi/service/UserService.java
+++ src/main/java/com/sda/clinicapi/service/UserService.java
@@ -1,31 +1,100 @@
 package com.sda.clinicapi.service;
 
 import com.sda.clinicapi.dto.UserDTO;
+import com.sda.clinicapi.exception.ResourceNotFoundException;
 import com.sda.clinicapi.exception.UsernameConflictException;
 import com.sda.clinicapi.mapper.UserMapper;
 import com.sda.clinicapi.model.User;
 import com.sda.clinicapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<UserDTO> findAll(PageRequest pageRequest) {
+        log.info("Fetching all users...");
+        List<UserDTO> users = userRepository.findAll(pageRequest).stream()
+                .map(userMapper::map)
+                .toList();
+
+        log.info("Fetch completed.");
+        return users;
+    }
+
+    public UserDTO findUserByUsername(String username) {
+        log.info("Fetching user with username '{}'.", username);
+        UserDTO userDTO = userRepository.findByUsername(username)
+                .map(user -> userMapper.map(user))
+                .orElseThrow(() -> {
+                    String message = "User with given username does not exists!";
+                    log.error(message);
+                    throw new ResourceNotFoundException(message);
+                });
+        log.info("Fetch completed.");
+        return userDTO;
+    }
 
     public void create(UserDTO userDTO) {
         String username = userDTO.getUsername();
+        log.info("Creating user with username: '{}'.", username);
+
         boolean exists = userRepository.existsById(username);
         if (exists) {
-            throw new UsernameConflictException("User with given username already exists!");
+            String message = "User with given username already exists!";
+            log.error(message);
+            throw new UsernameConflictException(message);
         }
 
         User user = userMapper.map(userDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setEnabled(true);
 
         userRepository.save(user);
+        log.info("Creating user completed.");
+    }
+
+    public void delete(String username) {
+        log.info("Removing user with username '{}'.", username);
+        checkIfUserExists(username);
+        userRepository.deleteById(username);
+        log.info("Removing user completed.");
+    }
+
+    public void update(String username, UserDTO userDTO) {
+        log.info("Updating user with username '{}'.", username);
+
+        if (!username.equals(userDTO.getUsername())) {
+            String message = "Usernames conflict!";
+            log.error(message);
+            throw new UsernameConflictException(message);
+        }
+
+        checkIfUserExists(username);
+        User user = userMapper.map(userDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        log.info("Updating user completed.");
+    }
+
+    private void checkIfUserExists(String username) {
+        boolean exists = userRepository.existsById(username);
+        if (!exists) {
+            String message = "User with given username does not exists!";
+            log.error(message);
+            throw new ResourceNotFoundException(message);
+        }
     }
 
 }
