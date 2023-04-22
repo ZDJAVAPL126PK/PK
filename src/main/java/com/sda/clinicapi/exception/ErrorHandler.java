ee3b9dbec8ce02b5a7be6cb9cd84a829b66d9373
szjozi <104274797+szjozi@users.noreply.github.com>
2023-04-22 15:18:23 +0200

    DT-33 Validation for user dto added (#29)

--- src/main/java/com/sda/clinicapi/exception/ErrorHandler.java
+++ src/main/java/com/sda/clinicapi/exception/ErrorHandler.java
@@ -2,11 +2,27 @@ package com.sda.clinicapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String errorMessage = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining("\n"));
+
+        ErrorResponse response = ErrorResponse.of(errorMessage, status);
+
+        return ResponseEntity.status(status).body(response);
+    }
 
     @ExceptionHandler(UsernameConflictException.class)
     public ResponseEntity<ErrorResponse> handleUsernameConflict(UsernameConflictException ex) {
@@ -18,7 +34,7 @@ public class ErrorHandler {
         return getErrorResponse(ex, HttpStatus.NOT_FOUND);
     }
 
-    private ResponseEntity<ErrorResponse> getErrorResponse(RuntimeException ex, HttpStatus status) {
+    private ResponseEntity<ErrorResponse> getErrorResponse(Exception ex, HttpStatus status) {
         return ResponseEntity.status(status).body(ErrorResponse.of(ex, status));
     }
 }
